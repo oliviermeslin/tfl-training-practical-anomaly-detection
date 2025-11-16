8ec54a722ffac0e0bd876df4d091a0a1a780181b
oliviermeslin <olivier.meslin@insee.fr>
2025-11-16 21:41:47 +0000

    Move to uv

--- init2.sh
+++ init2.sh
@@ -1,14 +1,9 @@
 cd tfl-training-practical-anomaly-detection
 
-# Create a virtual environment with uv specifying Python 3.11
-uv venv --python 3.11 myenv
+# uv init
+# uv add -r requirements.txt
 
-# Activate the virtual environment
-source myenv/bin/activate
-
-# Install packages into the created venv using uv pip or uv add
-uv pip install -r requirements.txt
-
+uv sync 
 cd ..
 mkdir -p .vscode
-echo '{"python.defaultInterpreterPath": ".tfl-training-practical-anomaly-detection/myenv/bin/python"}' >> .vscode/settings.json
+echo '{"python.defaultInterpreterPath": ".tfl-training-practical-anomaly-detection/.venv/bin/python"}' >> .vscode/settings.json
