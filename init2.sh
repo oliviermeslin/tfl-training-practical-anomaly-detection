3a6b3e1269531c4f819d2753565e96ec947c1cbd
oliviermeslin <olivier.meslin@insee.fr>
2025-11-16 22:39:11 +0000

    Download data

--- init2.sh
+++ init2.sh
@@ -3,7 +3,14 @@ cd tfl-training-practical-anomaly-detection
 # uv init
 # uv add -r requirements.txt
 
+wget -qO - https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt install -y git-lfs
+git lfs install
+git lfs pull
+
 uv sync 
 cd ..
 mkdir -p .vscode
 echo '{"python.defaultInterpreterPath": ".tfl-training-practical-anomaly-detection/.venv/bin/python"}' >> .vscode/settings.json
+
+
